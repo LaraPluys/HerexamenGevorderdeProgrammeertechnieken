3f2035ae87d70e89f669aba0f01d69dfff107d5a
LaraPluys <61267694+LaraPluys@users.noreply.github.com>
2021-08-11 12:37:46 +0200

    login

---
+++ HerexamenGevorderdeProgrammeertechnieken/Components/BindablePasswordBox.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HerexamenGevorderdeProgrammeertechnieken.Components
+{
+    /// <summary>
+    /// Interaction logic for BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+        //Deze DependencyProperty bind niet default TwoWay, hier FrameworkPropertyMetadataOptions.BindsTwoWayByDefault definieren
+        //Je kan ook TwoWay binding definieren in [NAAM VAN LOGIN VIEW].xaml 
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(BindablePasswordBox),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = passwordBox.Password;
+        }
+    }
+}
--- HerexamenGevorderdeProgrammeertechnieken/ViewModels/UserControlLoginViewModel.cs
+++ HerexamenGevorderdeProgrammeertechnieken/ViewModels/UserControlLoginViewModel.cs
@@ -4,35 +4,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace HerexamenGevorderdeProgrammeertechnieken.ViewModels
 {
     public class UserControlLoginViewModel : BasisViewModel
     {
         public INavigator Navigate => UpdateViewModel.Navigate;
-        public override string this[string columnName] => throw new NotImplementedException();
+
+        private string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public override string this[string columnName]
+        {
+            get
+            {
+                return "";
+            }
+        }
 
         public override bool CanExecute(object parameter)
         {
-            switch (parameter)
-            {
-                default:
-                    return false;
-                case "Login":
-                    return true;
-
-            }
+            return true;
         }
 
         public override void Execute(object parameter)
         {
-            switch (parameter)
+            switch (parameter.ToString())
             {
                 case "Login":
-
+                    this.Login();
                     break;
-
             }
         }
+
+        public void Login()
+        {
+            MessageBox.Show($"Username: {this.Username}\nPassword: {this.Password}", "Info",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }       
     }
 }
